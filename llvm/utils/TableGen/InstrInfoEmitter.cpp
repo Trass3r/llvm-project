5b900067fa226e62b8cde5ddc4486806161bd9b7
Andreas Hollandt <trass3r@gmail.com>
2019-09-29 15:52:57 +0200

    use named enums

--- llvm/utils/TableGen/InstrInfoEmitter.cpp
+++ llvm/utils/TableGen/InstrInfoEmitter.cpp
@@ -1223,7 +1223,7 @@ void InstrInfoEmitter::emitEnums(raw_ostream &OS) {
     PrintFatalError("No instructions defined!");
 
   OS << "namespace " << Namespace << " {\n";
-  OS << "  enum {\n";
+  OS << "  enum InstrInfo {\n";
   unsigned Num = 0;
   for (const CodeGenInstruction *Inst : Target.getInstructionsByEnumValue())
     OS << "    " << Inst->TheDef->getName() << "\t= " << Num++ << ",\n";
@@ -1238,7 +1238,7 @@ void InstrInfoEmitter::emitEnums(raw_ostream &OS) {
   OS << "namespace llvm {\n\n";
   OS << "namespace " << Namespace << " {\n";
   OS << "namespace Sched {\n";
-  OS << "  enum {\n";
+  OS << "  enum InstrInfoSched {\n";
   Num = 0;
   for (const auto &Class : SchedModels.explicit_classes())
     OS << "    " << Class.Name << "\t= " << Num++ << ",\n";
