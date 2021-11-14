d46b6a84aa049c5d2aa4369035612f73db5336e6
Andreas Hollandt <trass3r@gmail.com>
2021-11-14 13:25:36 +0100

    [clangd] add field padding inlay hint

--- clang-tools-extra/clangd/unittests/InlayHintTests.cpp
+++ clang-tools-extra/clangd/unittests/InlayHintTests.cpp
@@ -1530,6 +1530,16 @@ TEST(TypeHints, Decltype) {
                   ExpectedHint{": int", "h"}, ExpectedHint{": int", "i"});
 }
 
+TEST(TypeHints, Padding) {
+  assertTypeHints(R"cpp(
+    struct Foo {
+      bool b$b[[;]]
+      int i;
+    };
+  )cpp",
+                  ExpectedHint{" (+3 padding)", "b"});
+}
+
 TEST(TypeHints, SubstTemplateParameterAliases) {
   llvm::StringRef Header = R"cpp(
   template <class T> struct allocator {};
