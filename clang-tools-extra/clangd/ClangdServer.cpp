7fb84c1fa42c14df98c5df83c4d8776c270b62a3
Trass3r <trass3r@gmail.com>
2022-09-02 05:56:39 +0200

    revert questionable parts of Codelens change

--- clang-tools-extra/clangd/ClangdServer.cpp
+++ clang-tools-extra/clangd/ClangdServer.cpp
@@ -366,7 +366,7 @@ ClangdServer::createConfiguredContextProvider(const config::Provider *Provider,
 
 void ClangdServer::removeDocument(PathRef File) {
   DraftMgr.removeDraft(File);
-  // WorkScheduler->remove(File);
+  WorkScheduler->remove(File);
 }
 
 void ClangdServer::codeComplete(PathRef File, Position Pos,
