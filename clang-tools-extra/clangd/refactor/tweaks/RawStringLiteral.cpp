5e67d17d80c08e3416ed4b91995372380e3fcf85
Andreas Hollandt <trass3r@gmail.com>
2022-10-07 11:48:36 +0200

    [clangd] use fine-grained code action kinds

--- clang-tools-extra/clangd/refactor/tweaks/RawStringLiteral.cpp
+++ clang-tools-extra/clangd/refactor/tweaks/RawStringLiteral.cpp
@@ -27,7 +27,7 @@ namespace {
 /// After:
 ///   printf(R"("a"
 /// b)");
-class RawStringLiteral : public Tweak {
+class RawStringLiteral final : public Tweak {
 public:
   const char *id() const final;
 
@@ -35,7 +35,7 @@ public:
   Expected<Effect> apply(const Selection &Inputs) override;
   std::string title() const override { return "Convert to raw string"; }
   llvm::StringLiteral kind() const override {
-    return CodeAction::REFACTOR_KIND;
+    return CodeAction::REFACTOR_REWRITE_KIND;
   }
 
 private:
