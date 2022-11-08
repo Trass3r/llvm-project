2a14e7667dd47e471d03a2e08262187fab2c21e0
Andreas Hollandt <trass3r@gmail.com>
2022-11-08 23:21:28 +0100

    [cmake] Enable PCH

---
+++ llvm/lib/Demangle/pch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <optional>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <variant>
+#include <vector>
