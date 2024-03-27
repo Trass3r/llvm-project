3c3ed7515b6a9a26fd8bbcb7137cd88a93669ea0
Trass3r <trass3r@gmail.com>
2024-03-28 00:36:40 +0100

    move demangle

--- llvm/lib/Passes/StandardInstrumentations.cpp
+++ llvm/lib/Passes/StandardInstrumentations.cpp
@@ -240,7 +240,7 @@ std::string getIRName(Any IR) {
     return M->getModuleIdentifier();
 
   if (const auto *F = unwrapIR<Function>(IR))
-    return demangle(F->getName().str());
+    return F->getName().str();
 
   if (const auto *C = unwrapIR<LazyCallGraph::SCC>(IR))
     return C->getName();
@@ -1580,7 +1580,7 @@ void TimeProfilingPassesHandler::registerCallbacks(
 }
 
 void TimeProfilingPassesHandler::runBeforePass(StringRef PassID, Any IR) {
-  timeTraceProfilerBegin(PassID, getIRName(IR));
+  timeTraceProfilerBegin(PassID, demangle(getIRName(IR)));
 }
 
 void TimeProfilingPassesHandler::runAfterPass() { timeTraceProfilerEnd(); }
